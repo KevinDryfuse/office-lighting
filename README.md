8147f63f066265f44f4525d3b97e8b8fbf686499
Kevin Dryfuse <kevin@kevindryfuse.com>
2021-06-20 23:12:11 -0400

    Adds diagrams and basics to readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # office-lighting
 Work from home office is boring, let's add some lighting and effects to make it a little less boring
+
+## Prototype Layout
+![Image of prototype](/images/diagram.PNG)
+
+## Parts List
+
+ - [Arduino Micro](https://www.google.com)
+ - Breadboard
+ - 330Ω Resistor
+ - 1000µF electrolytic capacitor
+ - [Membrane Switch Module (4x4)](https://tinkersphere.com/buttons-switches/762-adhesive-keypad-membrane-matrix-3x4.html)
+ - [Power supply](https://www.google.com)
+ - [Addressable LED Strip](https://www.google.com)
+
+## Major outstanding issues
+- Slight flicker of lights at ends when full white at full brightness
+- Add all sorts of effects
+    - Dimming
+    - Changing colors
+    - Rainbow
+    - etc
+- Add sound level meter and cause lights to change color and brightness based on sound levels
--- arduino-source/arduino-source.ino
+++ arduino-source/arduino-source.ino
@@ -1,5 +1,18 @@
+#include <Adafruit_NeoPixel.h>
 #include <Keypad.h>
 
+// LED strip
+#define LED_PIN 10
+#define LED_COUNT 400
+#define LED_COUNT_TO_USE 212
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+int red = 0;
+int green = 0;
+int blue = 255;
+int curr_brightness = 255;
+
+
+// 4x4 Membrane Keypad
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 //define the cymbols on the buttons of the keypads
@@ -17,6 +30,11 @@ Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS
 void setup() {
   Serial.begin(9600);
   Serial.println("Welcome to your new office lighting!");  
+  setup_strip();
+  Serial.println(red);  
+  Serial.println(green);  
+  Serial.println(blue);  
+  set_strip_to_solid_color(strip.Color(red, green, blue), 50);
 }
 
 void loop() {
@@ -24,4 +42,26 @@ void loop() {
   if (customKey){
     Serial.println("You pressed the " + String(customKey) + " key!");   
   }
+}
+
+void setup_strip() {
+  strip.begin(); // INITIALIZE NeoPixel strip object
+  clear_strip();
+  strip.setBrightness(curr_brightness);
+}
+
+void clear_strip() {
+  Serial.println("Clearing all LEDs on strip");
+  for (int x = 0; x < LED_COUNT; x++) {
+      strip.setPixelColor(x, strip.Color(0, 0, 0));
+  }
+  strip.show();
+}
+
+void set_strip_to_solid_color(uint32_t color, int brightness) {
+  for (int led = 0; led < LED_COUNT_TO_USE ; led++) {
+    strip.setPixelColor(led, color);
+  }
+  strip.setBrightness(brightness);
+  strip.show();
 }
